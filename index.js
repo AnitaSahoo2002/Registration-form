9776cab741dbe4ded84fdea55b33288d8da1643c
Anita <anitasahoo0303@gmail.com>
2023-11-05 22:33:36 +0530

    deleted nodemodules

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
+require("dotenv").config();
 const express=require("express");
 const path=require("path");
 const app=express();
-const PORT=8001;
-const userRoute=require("./routes/user");
-const mongoose=require("mongoose");
+const PORT=process.env.PORT || 8001;
 
-mongoose.connect("mongodb://127.0.0.1:27017/form")
-.then(()=>console.log("mongodb connected"));
+const userRoute=require("./routes/user");
+
+var mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => {
+      console.log('MongoDB connected');
+    })
+    .catch((error) => {
+      console.error('Error connecting to MongoDB:', error);
+    });
+
 
 //middlewares
 app.set("view engine","ejs");
